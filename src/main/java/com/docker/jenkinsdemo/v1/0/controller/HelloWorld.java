80d8b74bf545e1f2e68bc21a2085b2ba48fbb174
Avinash <avinashtadikonda123@gmail.com>
2025-08-20 19:54:25 +0530

    package updated

--- src/main/java/com/docker/jenkinsdemo/v1/0/controller/HelloWorld.java
+++ src/main/java/com/docker/jenkinsdemo/v1/0/controller/HelloWorld.java
@@ -1,4 +1,4 @@
-package com.docker.demo.controller;
+package com.docker.jenkinsdemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/test/java/com/docker/jenkinsdemo/v1/0/DemoApplicationTests.java
+++ src/test/java/com/docker/jenkinsdemo/v1/0/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.docker.demo;
+package com.docker.jenkinsdemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
